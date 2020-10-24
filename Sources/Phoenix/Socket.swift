fe2f4eb95f9469890c9f5c165173bcc05b7a76c2
dsrees <drees@clustertruck.com>
2020-10-23 22:03:17 -0400

    Creating NSURL Transport

---
+++ Sources/Phoenix/Socket.swift
@@ -0,0 +1,32 @@
+//
+//  Socket.swift
+//  SwiftPhoenixClient
+//
+//  Created by Daniel Rees on 10/23/20.
+//  Copyright © 2020 SwiftPhoenixClient. All rights reserved.
+//
+
+import Foundation
+
+
+/**
+ ## Socket Connection
+ A single connection is established to the server and channels are multiplexed
+ over the connection.
+ 
+ Connect to the server using the `Socket` class
+ 
+ Example:
+
+ ```swift
+ let socket = new Socket("/socket") { ["user_token": "Bearer abc123"] }
+ socket.connect()
+ ```
+ */
+public class Socket {
+  func go() {
+    
+    
+    
+  }
+}
