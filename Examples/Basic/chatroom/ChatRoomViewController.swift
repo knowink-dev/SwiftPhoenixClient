7751dd691c6e023eb11902c9558db6c7739a8c7d
dsrees <daniel.rees18@gmail.com>
2020-12-30 10:07:01 -0500

    ChatRoom example sending and receving

--- Examples/Basic/chatroom/ChatRoomViewController.swift
+++ Examples/Basic/chatroom/ChatRoomViewController.swift
@@ -12,6 +12,11 @@ import RxCocoa
 import SwiftPhoenixClient
 import RxSwiftPhoenixClient
 
+struct Shout {
+  let name: String
+  let message: String
+}
+
 class ChatRoomViewController: UIViewController {
   
   // MARK: - Child Views
@@ -19,18 +24,21 @@ class ChatRoomViewController: UIViewController {
   @IBOutlet weak var tableView: UITableView!
   
   // MARK: - Attributes
+  private let username: String = "ChatRoom"
   private let socket = Socket("https://phxchat.herokuapp.com/socket/websocket")
-  private let topic: String = "rooms:lobby"
+  private let topic: String = "room:lobby"
   
   private var lobbyChannel: Channel?
+  private var shouts: [Shout] = []
   
-
   private let disposeBag = DisposeBag()
   
   
   // MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    self.tableView.dataSource = self
 
     
     // Setup the socket to receive open/close events
@@ -52,15 +60,20 @@ class ChatRoomViewController: UIViewController {
     // Setup the Channel to receive and send messages
     let channel = socket.channel(topic, params: ["status": "joining"])
     channel.rx
-      .on("join")
+      .on("shout")
+      .observeOn(MainScheduler.asyncInstance)
       .subscribe( onNext: { (message) in
         let payload = message.payload
         guard
-            let username = payload["user"],
-            let body = payload["body"] else { return }
-        let newMessage = "[\(username)] \(body)"
-        print("New mesage received: \(newMessage)")
-      
+            let name = payload["name"] as? String,
+            let message = payload["message"] as? String else { return }
+        
+        let shout = Shout(name: name, message: message)
+        self.shouts.append(shout)
+        
+        let indexPath = IndexPath(row: self.shouts.count - 1, section: 0)
+        self.tableView.reloadData() //reloadRows(at: [indexPath], with: .automatic)
+        self.tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
       }).disposed(by: disposeBag)
     
     // Now connect the socket and join the channel
@@ -80,12 +93,36 @@ class ChatRoomViewController: UIViewController {
   
   // MARK: - IB Actions
   @IBAction func onExitButtonPressed(_ sender: Any) {
+    self.socket.disconnect()
     self.navigationController?.popViewController(animated: true)
   }
   
   
   @IBAction func onSendButtonPressed(_ sender: Any) {
+    // Create and send the payload
+    let payload = ["name": username, "message": messageInput.text!]
+    self.lobbyChannel?.push("shout", payload: payload)
     
+    // Clear the text intput
+    self.messageInput.text = ""
+  }
+}
+
+
+extension ChatRoomViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.shouts.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "shout_cell")
+    
+    let shout = self.shouts[indexPath.row]
+    
+    cell.textLabel?.text = shout.message
+    cell.detailTextLabel?.text = shout.name
+    
+    return cell
   }
   
   
